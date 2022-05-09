39a67f8d976f60e406c37e56c3bfdbc7ba308c7b
Sergey Aleshin <sergey.g.aleshin@gmail.com>
2022-05-09 20:50:29 +0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+(function () {
+    const canvasId = "canvas";
+
+    window.addEventListener("load", init);
+
+    function init () {
+        let options = getInitialOptions();
+
+        createCanvas(options);
+
+        initHandlers();
+    }
+
+    function initHandlers () {
+        window.addEventListener("keyup", function (ev) {
+            console.log(ev);
+        });
+    }
+
+    function getCanvas () {
+        return document.getElementById(canvasId);
+    }
+
+    function createCanvas (options) {
+        var oldCanvas = getCanvas();
+
+        if (oldCanvas) {
+            oldCanvas.remove();
+        }
+
+        let canvas = document.createElement("canvas");
+        canvas.id = canvasId;
+        canvas.width = options.width;
+        canvas.height = options.height;
+
+        document.body.append(canvas);
+    }
+
+    function getInitialOptions () {
+        let options = {
+            width: window.visualViewport.width - 0,
+            height: window.visualViewport.height - 4
+        };
+
+        return options;
+    }
+})();
